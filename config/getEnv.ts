86b1c94837c84f74048a1a832bf51c08807f8681
Valentyn Kvasnytskyi <valentyn.k@sporthub.tech>
2024-11-04 08:58:42 +0200

    fix getEnv

--- config/getEnv.ts
+++ config/getEnv.ts
@@ -10,10 +10,8 @@ export const getEnv = (key: string, defaultValue?: string) => {
   }
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
   if (import.meta.env) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
     return (import.meta.env as any)[key] || defaultValue;
   }
   return defaultValue;
